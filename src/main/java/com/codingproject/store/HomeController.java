d7c9d6ea93e65c5e8848a3f04d744ee1c7546256
ibrahimisgreat109 <ibrahimsameer109@gmail.com>
2026-08-07 20:56:07 -0700

    Initial commit

---
+++ src/main/java/com/codingproject/store/HomeController.java
@@ -0,0 +1,19 @@
+package com.codingproject.store;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/index.html";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "redirect:/about.html";
+    }
+}
